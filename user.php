d5f7724e0ec69e743793e435275b6e8efc752eb1
Yeet <107809331+y3eet@users.noreply.github.com>
2025-02-12 13:41:22 +0800

    Update user.php

--- user.php
+++ user.php
@@ -8,6 +8,7 @@ if ($user["role"] !== 'user') {
 <!DOCTYPE html>
 <html lang="en">
 
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -15,15 +16,55 @@ if ($user["role"] !== 'user') {
 
 </head>
 
+<style>
+    body,
+    h1,
+    label,
+    a,
+    button {
+        font-family: Arial, sans-serif;
+    }
+
+
+    .container {
+        display: flex;
+        height: 80vh;
+        width: 95vw;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+    }
+
+    .input {
+        width: 400px;
+        height: 30px;
+        font-size: 15px;
+    }
+
+    .logout-btn {
+        background-color: rgb(175, 76, 76);
+        border: none;
+        color: white;
+        padding: 8px 16px;
+        text-align: center;
+        text-decoration: none;
+        font-size: 18px;
+        cursor: pointer;
+        border-radius: 8px;
+        width: 100px;
+        height: 50px;
+    }
+</style>
+
 <body>
     <h1>User Page</h1>
-    <?php
 
-    echo $user["username"];
-    ?>
-    <form action="user.php" method="POST">
-        <button type="submit" name="logout" value="logout">Logout</button>
-    </form>
+    <div>
+        <form class="container" action="user.php" method="POST">
+            <h1>Welcome, <?= $user["username"] ?>!</h1>
+            <button type="submit" name="logout" value="logout" class="logout-btn">Logout</button>
+        </form>
+    </div>
 </body>
 
 </html>
