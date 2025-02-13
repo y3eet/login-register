098484d144c2f80938ceeb593c4527be7c6100e1
Yeet <107809331+y3eet@users.noreply.github.com>
2025-02-13 16:57:46 +0800

    refactor

--- ajax/views/users_table.php
+++ ajax/views/users_table.php
@@ -2,29 +2,42 @@
 include("../../database.php");
 include("../../session.php");
 
-
-$current_user = get_session();
-if ($current_user["role"] !== 'admin') {
-    header("Location: ../../unauthorized.php");
-}
-
-// table that will be rendered
 $result = $conn->query("SELECT * FROM users");
-echo '<tr>
+$current_username = get_session()["username"];
+function disable_edit(string $username, string $current_username)
+{
+    return $current_username === $username ? 'disabled' : '';
+}
+?>
+<!-- table that will be rendered -->
+<thead>
+    <tr>
         <th>ID</th>
         <th>Username</th>
         <th>Role</th>
         <th>Date Created</th>
         <th>Action</th>
-    </tr>';
+    </tr>
+</thead>
 
-while ($user = $result->fetch_assoc()) {
-    echo "<tr>";
-    echo "<td>" . $user["id"] . "</td>";
-    echo "<td>" . $user["username"] . "</td>";
-    echo "<td>" . $user["role"] . "</td>";
-    echo "<td>" . $user["created_at"] . "</td>";
-    echo '<td><button class="edit-btn" data-id="' . $user["id"] .
-        '" data-username="' . $user["username"] . '" data-role="' . $user["role"] . '">Edit</button></td>';
-    echo "</tr>";
-}
+<tbody>
+    <?php
+    while ($user = $result->fetch_assoc()) {
+    ?>
+        <tr>
+            <td><?= $user["id"]; ?></td>
+            <td><?= $user["username"]; ?></td>
+            <td><?= $user["role"]; ?></td>
+            <td><?= $user["created_at"]; ?></td>
+            <td>
+                <button id="editBtn" <?= disable_edit($user["username"], $current_username) ?>
+                    data-bs-toggle="modal" data-bs-target="#editUserModal"
+                    class="btn btn-success" data-id="<?= $user["id"]; ?>"
+                    data-username="<?= $user["username"]; ?>"
+                    data-role="<?= $user["role"]; ?>">Edit</button>
+            </td>
+        </tr>
+    <?php
+    }
+    ?>
+</tbody>
